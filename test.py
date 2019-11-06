e4a2ab11ff18eab2631a48ee98aa335323be2181
Diego R. Gomez <rga.diego@gmail.com>
2019-11-06 13:49:38 +0100

    Started working on plotting circle

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #print("Hello world!")
 
@@ -11,5 +12,15 @@ def Area(r):
 	A = np.pi*r**2
 	return A
 
+#def PlotCirlce(r):
+#	theta = np.linspace(0, 2*np*pi, num=1E3)
+#	x = r*np.cos(theta)
+#	y = r*np.sin(theta)
+#	fig, axes = plt.subplots()
+#
+#	axes.plot(x, y)
+
+
 #r=3
+
 #print(Area(r))
